98ddf28e0846b630db312d185b541a89a01be655
James Carter <jwcarter@gmail.com>
2016-02-04 20:16:16 -0500

    Extended MWC32 L3 generator to generate 64-bit numbers by XOR'ing the 64-bit result of the MWC with a rotated version of itself.
    Signed-off-by: James Carter <jwcarter@gmail.com>

--- rnd_mwc32_l3_r.c
+++ rnd_mwc32_l3_r.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <float.h>
 
-/* 32 bit Multiply-With-Carry (MWC) with Lag 3
+/* 32 bit Multiply-With-Carry (MWC) with Lag 3 with rotation
  *
  * Author: James Carter
  *
@@ -25,6 +25,20 @@
  * Period: ~2^127
  */
 
+#define A 4293666429ULL
+
+#define K1 4110542893ULL /* 59443 * 69151 = 0xF501E82D */
+#define K2 3667735229ULL /* 51407 * 71347 = 0xDA9D32BD */
+#define K3 4068774709ULL /* 46399 * 87691 = 0xF2849335 */
+#define K4 3322924477ULL /* 44939 * 73943 = 0xC60FCDBD */
+
+#define L52(x) ((x)&0xFFFFFFFFFFFFFUL)
+#define H32(x) ((x)>>32)
+#define L32(x) ((x)&0xFFFFFFFFUL)
+#define L31(x) ((x)&0x7FFFFFFFUL)
+#define L30(x) ((x)&0x3FFFFFFFUL)
+#define RL64(x,r) ((x)<<(r) | (x)>>(64-(r)))
+
 struct rnd {
 	uint32_t s1;
 	uint32_t s2;
@@ -32,19 +46,14 @@ struct rnd {
 	uint32_t c;
 };
 
-#define A 4293666429ULL
-
-#define L32(x) ((x)&0xFFFFFFFFUL)
-#define SR32(x) ((x)>>32)
-
-static inline uint32_t next(struct rnd *rnd)
+static inline uint64_t next(struct rnd *rnd)
 {
 	uint64_t x = rnd->s1*A+rnd->c;
 	rnd->s1 = rnd->s2;
 	rnd->s2 = rnd->s3;
-	rnd->s3 = L32(x);
-	rnd->c = SR32(x);
-	return rnd->s3;
+	rnd->s3 = x;
+	rnd->c = H32(x);
+	return x + RL64(x,13);
 }
 
 /*
@@ -63,14 +72,17 @@ struct rnd *rnd_new()
 
 void rnd_init(struct rnd *rnd, unsigned long seed)
 {
-	rnd->s1 = seed;
-	rnd->s2 = 2147483647UL;  /* 1st prime < 2^31 */
-	rnd->s3 = 1073741789UL;  /* 1st prime < 2^30 */
-	rnd->c  =  536870909UL;  /* 1st prime < 2^29 */
-	next(rnd);
-	next(rnd);
-	next(rnd);
-	next(rnd);
+	int i;
+	uint32_t x1 = ((seed + K1) * K2) + K3;
+	uint32_t x2 = ((seed + x1) * K3) + K4;
+	uint32_t x3 = ((seed + x2) * K4) + K1;
+	uint32_t x4 = ((seed + x3) * K1) + K2;
+	rnd->s1 = L31(x1);
+	rnd->s2 = L31(x2);
+	rnd->s3 = L31(x3);
+	rnd->c  = L30(x4);
+	for (i=0; i<4; i++)
+		next(rnd);
 }
 
 unsigned rnd_get_state_size()
@@ -103,40 +115,41 @@ void rnd_free(struct rnd *rnd)
  * Random Numbers
  */
 
-#define UMAX 4294967295ULL /* 2^32-1 */
+#define UMAX32 4294967295ULL       /* 2^32-1 */
+#define UMAX52 4503599627370495ULL /* 2^52-1 */
 #define HIGH_OPEN (1.0 - DBL_EPSILON)
 #define LOW_OPEN (DBL_EPSILON)
-#define CLOSED(x) (((double)(x))/((double)(UMAX)))
+#define CLOSED(x) (((double)(x))/((double)(UMAX52)))
 #define OPEN1(x) (CLOSED(x)*(HIGH_OPEN-LOW_OPEN)+LOW_OPEN)
 #define OPEN2(x) (CLOSED(x)*(HIGH_OPEN-LOW_OPEN)+2.0*LOW_OPEN)
 #define OPENn(x,n) (CLOSED(x)*(HIGH_OPEN-LOW_OPEN)+(n)*LOW_OPEN)
 
 unsigned long rnd_number_max()
 {
-	return UMAX;
+	return UMAX32;
 }
 
 unsigned long rnd_number(struct rnd *rnd)
 {
-	return next(rnd);
+	return L32(next(rnd));
 }
 
 double rnd_closed(struct rnd *rnd)
 {
 	/* Return double [0,1] in continuous uniform distribution */
-	return CLOSED(next(rnd));
+	return CLOSED(L52(next(rnd)));
 }
 
 double rnd_double(struct rnd *rnd)
 {
 	/* Return double (0,1) in continuous uniform distribution */
-	return OPEN1(next(rnd));
+	return OPEN1(L52(next(rnd)));
 }
 
 double rnd_double_2(struct rnd *rnd)
 {
 	/* Return double (0,2) in continuous triangular distribution */
-	return OPEN2((double)next(rnd)+(double)next(rnd));
+	return OPEN2((double)L52(next(rnd))+(double)L52(next(rnd)));
 }
 
 double rnd_double_n(struct rnd *rnd, unsigned n)
@@ -145,7 +158,7 @@ double rnd_double_n(struct rnd *rnd, unsigned n)
 	unsigned i = n;
 	double x = 0;
 	for (i = 0; i<n; i++) {
-		x += next(rnd);
+		x += L52(next(rnd));
 	}
 	return OPENn(x,n);
 }
